2906b8532150a6b437c87fb84397b113a34ebed6
Samuel Christopher <thesamchris@gmail.com>
2017-09-07 16:15:29 -0700

    Added drawer

--- src/QueProApp/QueProApp.js
+++ src/QueProApp/QueProApp.js
@@ -4,7 +4,9 @@ import MainView from  './MainView'
 import LiveMonitor from '../LiveMonitor/LiveMonitor'
 import UserRoute from  '../RouteTypes/UserRoute'
 import GuestRoute from  '../RouteTypes/GuestRoute'
+import MenuItem from 'material-ui/MenuItem'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import './QueProApp.css'
@@ -14,8 +16,12 @@ class QueProApp extends Component {
   constructor() {
     super()
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      open: false
     }
+
+    this.handleToggle = this.handleToggle.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   componentWillMount() {
@@ -32,13 +38,34 @@ class QueProApp extends Component {
     })
   }
 
+  handleToggle() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  handleClose() {
+    this.setState({
+      open: false
+    })
+  }
+
   render() {
     return (
       <div>
         <AppBar
           title="QuePro"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          showMenuIconButton={this.state.isAuthenticated}
+          onLeftIconButtonTouchTap={this.handleToggle}
         />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})} >
+          <MenuItem onTouchTap={this.handleClose}>Login</MenuItem>
+        </Drawer>
         <div className="container">
           <UserRoute user={this.state.user} isAuthenticated={this.state.isAuthenticated} path={`${this.props.match.url}`} exact component={MainView} />
           <UserRoute user={this.state.user} isAuthenticated={this.state.isAuthenticated} path={`${this.props.match.url}another`} exact component={LiveMonitor} />
